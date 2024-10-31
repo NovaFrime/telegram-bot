de37ff4c0877c50e25510b3caabf7d02ba9a55d3
Lê Thành Danh - Nova <ITITWE23014@student.hcmiu.edu.vn>
2024-10-31 13:53:00 +0700

    Danh | update faq

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 const app = express();
 
-const options = {
+const play_options = {
     reply_markup: {
         inline_keyboard: [
             [
@@ -34,18 +34,73 @@ const options = {
                     text: "🔄 Refresh",
                     callback_data: 'refresh' 
                 },
+            ],
+            [
+                {
+                    text: "❔ FAQ & Help",
+                    callback_data: 'faq_help' 
+                },
+                
             ]
         ],
     },
 };
+const help_options = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                {
+                    text: "Bạn muốn nhận quà bằng cách nào?",
+                    callback_data: 'nhanqua' 
+                },
+                {
+                    text: "Quy đổi phẩn thưởng như thế nào?",
+                    callback_data: 'doithuong' 
+                },
+            ],
+        ],
+    },
+};
 
+const nhanqua_options = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                {
+                    text: "Telegram",
+                    callback_data: 'nhanqua_telegram'
+                },
+                {
+                    text: "Facebook",
+                    callback_data: 'nhanqua_facebook'
+                },
+            ],
+        ],
+    },
+};
 
+const doithuong_options = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                {
+                    text: "Telegram",
+                    callback_data: 'doithuong_telegram'
+                },
+                {
+                    text: "Facebook",
+                    callback_data: 'doithuong_facebook'
+                },
+            ],
+        ],
+    },
+};
 
 let groupChatIds = [];
 
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Click below to play the game!", options);
+    bot.sendMessage(msg.chat.id, "Click below to play the game!", play_options);
 });
 
 
@@ -61,14 +116,14 @@ bot.onText(/\/help/, (msg) => {
 
 
 bot.onText(/\/play/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Click the button below to play the game!", options);
+    bot.sendMessage(msg.chat.id, "Click the button below to play the game!", play_options);
 });
 
 
 
 cron.schedule('0 10 * * *', () => {
     groupChatIds.forEach(chatId => {
-        bot.sendMessage(chatId, "Don't forget to share the Wheel of Fortune bot with your friends!", options);
+        bot.sendMessage(chatId, "Don't forget to share the Wheel of Fortune bot with your friends!", play_options);
     });
 });
 
@@ -102,16 +157,40 @@ bot.on("callback_query", (callbackQuery) => {
     const userId = callbackQuery.from.id;
     const data = callbackQuery.data;
 
-    if (data === 'share_link') {
-        
-        bot.sendMessage(userId, "Share below message!");
-        bot.sendMessage(userId, `Play Wheel of Fortune here to earn real life prizes!!! JOIN NOW: ${shareLink}`);
-    } else if (data === 'stats') {
-        bot.sendMessage(userId, "Here are your game stats!");
-    } else if (data === 'refresh') {
-        bot.sendMessage(userId, "Refreshing your game data...", options);
-    } else {
-        bot.sendMessage(userId, "Click below to play the game!", options);
+    switch (data) {
+        case 'faq_help':
+            bot.sendMessage(userId, "Bạn muốn hỏi gì ?", help_options);
+            break;
+        case 'share_link':
+            bot.sendMessage(userId, "Share below message!");
+            bot.sendMessage(userId, `Play Wheel of Fortune here to earn real life prizes!!! JOIN NOW: ${shareLink}`);
+            break;
+        case 'stats':
+            bot.sendMessage(userId, "Here are your game stats!");
+            break;
+        case 'refresh':
+            bot.sendMessage(userId, "Refreshing your game data...", play_options);
+            break;
+        case 'nhanqua':
+            bot.sendMessage(userId, "Chọn nền tảng để nhận quà:", nhanqua_options);
+            break;
+        case 'doithuong':
+            bot.sendMessage(userId, "Chọn nền tảng để đổi thưởng:", doithuong_options);
+            break;
+        case 'nhanqua_telegram':
+            bot.sendMessage(userId, "Bạn đã chọn nhận quà qua Telegram, hãy liên hệ hỗ trợ viên để nhận quà!");
+            break;
+        case 'nhanqua_facebook':
+            bot.sendMessage(userId, "Bạn đã chọn nhận quà qua Facebook, hãy nhắn cho page để nhận quà!");
+            break;
+        case 'doithuong_telegram':
+            bot.sendMessage(userId, "Bạn đã chọn đổi thưởng qua Telegram, hãy liên hệ hỗ trợ viên để đổi thưởng!");
+            break;
+        case 'doithuong_facebook':
+            bot.sendMessage(userId, "Bạn đã chọn đổi thưởng qua Facebook, hãy nhắn cho page để đổi thưởng!");
+            break;
+        default:
+            bot.sendMessage(userId, "I didn't understand that. Try /play or /help.");
     }
 });
 
